7cda5660ef0b8cc87d845f689e47952962480d14
divinites <divinites@gmail.com>
2016-07-22 15:26:25 +0100

    amend install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-sudo apt-get install wget curl python-pip tmux thefuck git vim-nox-py2
+apt-get install wget curl python-pip tmux thefuck git vim-nox-py2
 gem install briefcase
 briefcase git remote add origin https://github.com/divinites/dotfiles
 briefcase git pull origin linux
@@ -8,9 +8,9 @@ briefcase git submodule update --init
 pip install powerline-status
 cd ~ && git clone https://github.com/powerline/fonts && ./fonts/install.sh && rm -rf fonts
 curl https://j.mp/spf13-vim3 -L > spf13-vim.sh && sh spf13-vim.sh
+rm spf13-vim.sh
 cd ~ && rm .vimrc.local
-briefcase sync
 briefcase generate
-
+briefcase sync
 
 
