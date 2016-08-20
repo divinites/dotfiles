30d4e445cf96458da2d3e3c9878049e2cf0fa84f
Divinites <divinites@gmail.com>
2016-08-20 17:29:59 +0100

    osx install.sh

--- install.sh
+++ install.sh
@@ -1,16 +1,20 @@
 #! /bin/sh
-
-sudo apt-get install wget curl python-pip tmux git 
-sudo gem install briefcase
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew install ruby wget tmux git zsh
+brew install python --framework
+brew install python3 --framework
+gem install briefcase
+cd ~
+mkdir .dotfiles
 briefcase git remote add origin https://github.com/divinites/dotfiles
-briefcase git pull origin linux
+briefcase git pull origin master
 briefcase git submodule update --init
-sudo pip install powerline-status
+pip install powerline-status
 cd ~ && git clone https://github.com/powerline/fonts && ./fonts/install.sh && rm -rf fonts
+cd ~ && rm .vimrc.local
+briefcase sync
 curl https://j.mp/spf13-vim3 -L > spf13-vim.sh && sh spf13-vim.sh
 rm spf13-vim.sh
-cd ~ && rm .vimrc.local
 briefcase generate
 briefcase sync
-
 
